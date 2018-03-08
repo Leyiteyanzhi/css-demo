a7198d26a48fb0c9907138d87c4fad99a3a678da
litianzhi <979638192@qq.com>
2018-03-08 22:08:22 +0800

    css-demo

---
+++ demo7/file.js
@@ -0,0 +1,7 @@
+function shout() {
+    var beatles = Array("John","Paul","LiMing","Xiaohong");
+    for(var count = 0;count < beatles.length;count++){
+        alert(beatles[count])
+    }
+}
+shout();
